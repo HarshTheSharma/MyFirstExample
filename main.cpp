f64d00cc7f4709b34952c4e98455a10a32be0718
harshthesharma <harshthesharma@gmail.com>
2025-09-08 11:26:11 -0700

    updated strings to show operations, fixes #62.

--- main.cpp
+++ main.cpp
@@ -7,18 +7,18 @@ using std::endl;
 
 int main()
 {
-    cout << "Hi, please enter two whole numbers: ";
+    cout << "Enter two whole numbers: ";
 
     int x,y;
 
     cin >> x >> y;
-    cout << "Addition: " << x + y << endl;
-    cout << "Subtraction: " << x - y << endl;
-    cout << "Multiplication: " << x * y << endl;
-    cout << "Division: " << x / y << endl;
-    cout << "Remainder: " << x % y << endl;
-    cout << "Square Root: " << sqrt(x) << endl;
-    cout << "Square: " << pow(x, y) << endl;
+    cout << "Addition: " << x << " + " << y << " = " << x + y << endl;
+    cout << "Subtraction: " << x << " - " << y << " = "  << x - y << endl;
+    cout << "Multiplication: " << x << " * " << y << " = "  << x * y << endl;
+    cout << "Division: " << x << " / " << y << " = "  << x / y << endl;
+    cout << "Remainder: " << x << " % " << y << " = "  << x % y << endl;
+    cout << "Square Root: " << "sqrt(" << x << ") = " << sqrt(x) << endl;
+    cout << "Square: " << x << " ^ " << y << " = " << pow(x, y) << endl;
 
     return 0;
 }
