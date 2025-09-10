284a8f9e4c380b25ba3407c0217ab8ade1bdd11d
harshthesharma <harshthesharma@gmail.com>
2025-09-10 11:31:07 -0700

    cleanly merged newupstream version with previous pull request

--- main.cpp
+++ main.cpp
@@ -1,24 +1,21 @@
 #include <iostream>
 #include <cmath>
 
-using std::cin;
-using std::cout;
-using std::endl;
-
 int main()
 {
-    cout << "Enter two whole numbers: ";
+    std::cout << "THE FIRST EXAMPLE MATH DISPLAY!\n";
+    std::cout << "Enter two whole numbers: ";
 
     int x,y;
 
-    cin >> x >> y;
-    cout << "Addition: " << x << " + " << y << " = " << x + y << endl;
-    cout << "Subtraction: " << x << " - " << y << " = "  << x - y << endl;
-    cout << "Multiplication: " << x << " * " << y << " = "  << x * y << endl;
-    cout << "Division: " << x << " / " << y << " = "  << x / y << endl;
-    cout << "Remainder: " << x << " % " << y << " = "  << x % y << endl;
-    cout << "Square Root: " << "sqrt(" << x << ") = " << sqrt(x) << endl;
-    cout << "Square: " << x << " ^ " << y << " = " << pow(x, y) << endl;
+    std::cin >> x >> y;
+    std::cout << "Addition: " << x << " + " << y << " = " << x + y << std::endl;
+    std::cout << "Subtraction: " << x << " - " << y << " = "  << x - y << std::endl;
+    std::cout << "Multiplication: " << x << " * " << y << " = "  << x * y << std::endl;
+    std::cout << "Division: " << x << " / " << y << " = "  << x / y << std::endl;
+    std::cout << "Remainder: " << x << " % " << y << " = "  << x % y << std::endl;
+    std::cout << "Square Root: " << "sqrt(" << x << ") = " << sqrt(x) << std::endl;
+    std::cout << "Square: " << x << " ^ " << y << " = " << pow(x, y) << std::endl;
 
     return 0;
 }
